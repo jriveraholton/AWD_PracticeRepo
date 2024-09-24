c04dcc2d7a344ea4835581ad83d0efa27ffb97db
jriveraholton <164359247+jriveraholton@users.noreply.github.com>
2024-09-24 09:52:09 -0400

    First Commit with simple Ruby file in it.

---
+++ intro.rb
@@ -0,0 +1,20 @@
+#simple function definition
+def my_func(text)
+  puts "Functional text: " + text
+end
+
+
+#main begins here
+puts "Hello World!"
+
+#just some common arithmetic
+x = 7 + 10 % 3
+
+#classic while loop
+while x >= 0 do
+  puts x
+  x -= 1
+end
+
+#calling the function
+my_func("Wowza")
